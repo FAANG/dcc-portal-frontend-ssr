5e17cfd5433649314fa345c00bcd514ed45b1b31
Yogmatee Roochun <yroochun@ebi.ac.uk>
2024-07-29 22:35:14 +0100

    FAANG with ssr project - added validation analyses, experiments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -26,8 +26,8 @@ import {RulesetAnalysisComponent} from './rulesets/ruleset-analysis/ruleset-anal
 import {RulesetSampleComponent} from './rulesets/ruleset-sample/ruleset-sample.component';
 import {RulesetExperimentComponent} from './rulesets/ruleset-experiment/ruleset-experiment.component';
 import {ValidationSamplesComponent} from './validation/validation-samples/validation-samples.component';
-// import {ValidationExperimentsComponent} from './validation/validation-experiments/validation-experiments.component';
-// import {ValidationAnalysesComponent} from './validation/validation-analyses/validation-analyses.component';
+import {ValidationExperimentsComponent} from './validation/validation-experiments/validation-experiments.component';
+import {ValidationAnalysesComponent} from './validation/validation-analyses/validation-analyses.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
@@ -59,4 +59,6 @@ export const routes: Routes = [
   {path: 'ruleset/experiments', component: RulesetExperimentComponent},
   {path: 'ruleset/analyses', component: RulesetAnalysisComponent},
   {path: 'validation/samples', component: ValidationSamplesComponent},
+  {path: 'validation/experiments', component: ValidationExperimentsComponent},
+  {path: 'validation/analyses', component: ValidationAnalysesComponent},
 ];
